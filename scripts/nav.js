1608bf216800a8a27b7e5b8a3c993e80e360363f
emmadong05 <dong.emma05@gmail.com>
2026-04-11 16:50:35 -0400

    guide

--- scripts/nav.js
+++ scripts/nav.js
@@ -12,25 +12,13 @@
         <nav class="main-nav" aria-label="Main navigation">
           <ul class="menu">
             <li class="menu-item">
-              <a class="menu-label" href="${href("resources/index.html")}">Resources</a>
-              <ul class="dropdown" aria-label="Resources submenu">
-                <li><a href="${href("resources/mining-kit/index.html")}">Resource Subpage 1</a></li>
-                <li><a href="${href("resources/food-supplies/index.html")}">Resource Subpage 2</a></li>
-              </ul>
+              <a class="menu-label" href="${href("getting-started/index.html")}">Getting Started</a>
             </li>
             <li class="menu-item">
-              <a class="menu-label" href="${href("homes/index.html")}">Homes</a>
-              <ul class="dropdown" aria-label="Homes submenu">
-                <li><a href="${href("homes/starter-house/index.html")}">Home Subpage 1</a></li>
-                <li><a href="${href("homes/cliffside-base/index.html")}">Home Subpage 2</a></li>
-              </ul>
+              <a class="menu-label" href="${href("house/index.html")}">House Guide</a>
             </li>
             <li class="menu-item">
-              <a class="menu-label" href="${href("farms/index.html")}">Farms</a>
-              <ul class="dropdown" aria-label="Farms submenu">
-                <li><a href="${href("farms/wheat-farm/index.html")}">Farm Subpage 1</a></li>
-                <li><a href="${href("farms/mob-farm/index.html")}">Farm Subpage 2</a></li>
-              </ul>
+              <a class="menu-label" href="${href("farm/index.html")}">Farm Guide</a>
             </li>
           </ul>
         </nav>
